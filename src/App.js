f11a63781ab90780b364173fe655660c79ace6c7
derek101225 <derek101225@gmail.com>
2020-11-25 14:42:05 -0500

    derek

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SearchMovie from './Componets/SearchMovie/Search'
+import Top6 from './Componets/Top6/Top6';
 
 function App() {
   return (
     <div className="container">
     <h1 className='title'>MovieView Search</h1>
     <SearchMovie/>
+    <Top6 />
     </div>
   );
 }
---
+++ src/Componets/Top6/Top6.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+
+const Top6 = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() =>{
+        loadData();
+    }, []);
+
+        
+    const loadData = async () => {
+        const PopulerURL = 'https://api.themoviedb.org/3/movie/popular?api_key=58269892c382f28ba4692e1cab597755&language=en-US&page=1'
+
+        try {
+            const res = await fetch(PopulerURL);
+            const data  = await res.json();
+            setMovies(data.results)
+        }catch(err){
+            console.error(err);
+        }
+    }
+
+
+
+
+    return(
+        <div>
+        {movies.filter(movie => movie.poster_path).map(movie => (
+            <div key={movie.id}>
+               <img  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt='movie title' />
+                <div>
+                    <h3>{movie.title}</h3>
+                    <p><smal>RELEASE DATE: {movie.release_date}</smal></p>
+                    <p><smal>Rating: {movie.vote_average}</smal></p>
+                    <p>{movie.overview}</p>
+                </div>
+            </div>
+        ))}
+    </div>
+    )
+}
+
+
+export default Top6;
