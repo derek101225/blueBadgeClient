85cf362c2899651ee20c864be2a584eed613e7b4
derek101225 <derek101225@gmail.com>
2020-12-01 16:53:39 -0500

    derek

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import Movieview from './Componets/MovieView';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Auth from './Components/Auth/Auth'
-import UpComing from './Componets/UpComing';
-import TopRated from './Componets/TopRated';
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Movieview from "./Components/MovieView";
+import Auth from "./Components/Auth/Auth";
+import UpComing from "./Components/UpComing";
+import TopRated from "./Components/TopRated";
+import SignUp from "./Components/SignUp";
 
 function App() {
-  const [sessionToken, setSessionToken] = useState('')
+  const [sessionToken, setSessionToken] = useState("");
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      setSessionToken(localStorage.getItem('token'))
-      console.log(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
     }
-  }, [])
+  }, []);
 
-  console.log(sessionToken);
-  
   const updateToken = (newToken) => {
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
     setSessionToken(newToken);
-    console.log("sessionToken", newToken)
-  }
+    console.log(sessionToken);
+  };
 
-     
+  // const [token, setToken] = useState(undefined)
+
+  // const viewConductor =() => {
+  //   return token === undefined ? <Auth updateToken={updateToken} /> : <Movieview token = {token} />
+  // }
+
+  // const updateToken = (newToken) => {
+  //   setToken(newToken)
+  // }
+
   return (
-
     <div>
-
       <BrowserRouter>
-      <Switch>
-      
-      <Route path='/' exact component={Movieview} />
-      
-      <Route path='/Upcoming' exact component={UpComing} />
-      <Route path='/TopRated' exact component={TopRated} />
-      <Auth updateToken={updateToken}/>
-      <Route path='/Auth' exact component={Auth } />
-      
-      </Switch>
-    </BrowserRouter>
-    
-
+        <Switch>
+          <Route path="/" exact component={Movieview} />
+          <Route path="/Upcoming" exact component={UpComing} />
+          <Route path="/TopRated" exact component={TopRated} />
+          <Auth updateToken={updateToken} />
+          <Route path="/Auth" exact component={Auth} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
