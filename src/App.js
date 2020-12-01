567b11ad48511f6e098c7fb06580f9de8fdbb327
Nick Smolen <nasmolen@gmail.com>
2020-12-01 12:25:20 -0500

    starting fresh with new code from Tony

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import Movieview from './Componets/MovieView';
-import Auth from './Components/Auth/Auth'
-import { useState } from 'react'
-
-=======
 import React from 'react';
 import Movieview from './Componets/MovieView';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -11,7 +5,6 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import UpComing from './Componets/UpComing';
 import TopRated from './Componets/TopRated';
 import SignUp from './Componets/SignUp/SignUp';
->>>>>>> 3d1e332c9ab40f3bae7b98fa30fdc3451809dfa1
 
 
 
@@ -28,16 +21,6 @@ function App() {
   }
     
   return (
-<<<<<<< HEAD
-    
-
-    <div>
-    <button>{viewConductor()}</button>
-    <Movieview />
-    
-      
-
-=======
     <div>
     
     <BrowserRouter>
@@ -52,7 +35,6 @@ function App() {
       </Switch>
     </BrowserRouter>
     
->>>>>>> 3d1e332c9ab40f3bae7b98fa30fdc3451809dfa1
     </div>
   );
 }
